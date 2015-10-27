3dc255ba99f86b069a0c45beda89685ebd1e459e
Ayush Shukla <ayush.shukla92@gmail.com>
2015-10-27 13:42:38 +0530

    c++ template added

---
+++ template2.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <list>
+#include <stack>
+#include <vector>
+#include <map>
+// #include <unordered_map>
+#include <set>
+// #include <unordered_set>
+
+#include <cstdlib>
+#include <sstream>
+#include <cmath>
+#include <cctype>
+#include <cstdio>
+#include <cstring>
+
+
+using namespace std;
+
+typedef pair <int, int> PII;
+typedef vector <int> VI;
+typedef vector<vector<int> > VVI;
+typedef vector<vector<char> > VVC;
+typedef vector <bool> VB;
+typedef vector < pair<int,int> > VP;
+typedef vector <double> VD;
+typedef long long ll;
+typedef long double ld;
+
+// Input macros	
+#define SC(n)    scanf(" %c",&n)
+#define SD(n)    scanf("%d",&n)
+#define SF(n)    scanf("%f",&n)
+#define SLF(n)    scanf("%lf",&n)
+
+#define ALL(a)                      a.begin(), a.end()
+#define IN(a,b)                     ( (b).find(a) != (b).end())
+
+#define error(x) cerr << #x << " = " << (x) <<endl
+#define Error(a,b) cerr<<"( "<<#a<<" , "<<#b<<" ) = ( "<<(a)<<" , "<<(b)<<" )\n";
+#define foreach(v, c)               for( typeof( (c).begin()) v = (c).begin();  v != (c).end(); ++v)
+
+#define PB push_back
+#define PF push_front
+#define MP make_pair
+#define EL endl
+#define CO cout
+#define F first
+#define S second
+#define MAX 1000000007
+
+template <typename T>
+ void print(vector<T> &v) 
+{ 
+    for (int i = 0; i < v.size(); ++i)
+    {
+    	cout<<v[i]<<" ";
+    }
+    CO<<EL;
+} 
+// map<int,int>::iterator it;
+// 	foreach(it,m){					// iterator of map
+// 		cout<< (*it).F;
+// 	}
+//-----------------------------------------utilities end---------------------------
+
+int main()
+{
+	// int tCases;
+	// SD(tCases);
+	// while(tCases--){
+
+	// }
+	
+	return 0;
+}
